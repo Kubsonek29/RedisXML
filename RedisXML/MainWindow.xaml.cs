52df1e8a0ba57cfd51c2443a0bff8a035211e324
Jakub <jakub.iwicki@op.pl>
2023-10-26 21:02:21 +0200

    Add project files.

---
+++ RedisXML/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gender_bender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedisToXMLBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string[] filelines = RedisToXML.open();
+            foreach(var strings in filelines)
+            {
+                filetextLb.Content += strings + "\n";
+            }
+        }
+    }
+}
---
+++ RedisXML/RedisToXML.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gender_bender
+{
+    public class RedisToXML
+    {
+        public string FileName { get; set; }
+        public RedisToXML() { }
+
+        public static string[]? open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            openFileDialog.Title = "Choose file with redis commands";
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                string line = System.Text.Encoding.Default.GetString(System.IO.File.ReadAllBytes(openFileDialog.FileName));
+
+                string[] lines = line.Split(Environment.NewLine,
+                            StringSplitOptions.RemoveEmptyEntries);
+
+                return lines;
+            }
+            return null;
+        }
+    }
+}
